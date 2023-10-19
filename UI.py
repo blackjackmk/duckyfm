a131523aec0b50147c081e5c274f68725a84c4ed
MZ2137 <29977@s.pm.szczecin.pl>
2023-10-19 18:26:18 +0200

    commit

--- UI.py
+++ UI.py
@@ -1,10 +1,14 @@
 import tkinter
 import tkinter.messagebox
 import customtkinter
+from googletrans import Translator
+
+translator = Translator()
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
+### linijki 40-59 i 164-178 są do przejrzenia ###
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -25,20 +29,34 @@ class App(customtkinter.CTk):
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="DuckyFM", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        
         self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
         self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
         self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
         self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
         self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=self.sidebar_button_event)
         self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
+
+#-------------------------------------------------------------------------------------------------
+
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
-        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_label.grid(row=4, column=0, padx=20, pady=(10, 0))
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"], command=self.change_appearance_mode_event)
-        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
+        self.appearance_mode_optionemenu.grid(row=5, column=0, padx=20, pady=(10, 10))
+        
+        
+        self.color_theme_label = customtkinter.CTkLabel(self.sidebar_frame, text="Color Theme:", anchor="w")
+        self.color_theme_label.grid(row=6, column=0, padx=20, pady=(10, 0))
+        self.color_theme_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["blue", "green", "dark-blue"], command=self.change_default_color_theme)
+        self.color_theme_optionemenu.grid(row=7, column=0, padx=20, pady=(10, 10))
+        
+        
         self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
-        self.scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
+        self.scaling_label.grid(row=8, column=0, padx=20, pady=(10, 0))
         self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"], command=self.change_scaling_event)
-        self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
+        self.scaling_optionemenu.grid(row=9, column=0, padx=20, pady=(10, 20))
+
+#-------------------------------------------------------------------------------------------------
 
         # create main entry and button
         self.entry = customtkinter.CTkEntry(self, placeholder_text="CTkEntry")
@@ -143,12 +161,21 @@ class App(customtkinter.CTk):
         dialog = customtkinter.CTkInputDialog(text="Type in a number:", title="CTkInputDialog")
         print("CTkInputDialog:", dialog.get_input())
 
+#-------------------------------------------------------------------------------------------------
+
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
+    
+    
+    def change_default_color_theme(self, new_color_theme: str):
+        customtkinter.set_default_color_theme(new_color_theme)
+
 
     def change_scaling_event(self, new_scaling: str):
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         customtkinter.set_widget_scaling(new_scaling_float)
+        
+#-------------------------------------------------------------------------------------------------
 
     def sidebar_button_event(self):
         print("sidebar_button click")
@@ -157,3 +184,21 @@ class App(customtkinter.CTk):
 if __name__ == "__main__":
     app = App()
     app.mainloop()
+    
+    
+    
+    
+"""
+kodzik jako wzór na przyszłe tłumaczenie całości
+
+
+from googletrans import Translator
+
+text1 = "I want to buy bananas"
+
+translator = Translator()
+
+result = (translator.translate(text1, src='en', dest='pl')).text
+print(result)
+
+"""
