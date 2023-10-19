859e6e8c707304cb538340721b08c28e377a90bf
MZ2137 <29977@s.pm.szczecin.pl>
2023-10-19 15:40:28 +0200

    szyszka urgent x-large

--- elo.py
+++ elo.py
@@ -1,13 +1,24 @@
 import mysql.connector
+import os
 
-mydb = mysql.connector.connect(
-  host="185.253.218.123",
-  user="wwmdhvjb_student",
-  password="dzikaszyszk@2024",
-  database="wwmdhvjb_duckyfm"
-)
+if os.path.isfile("dzika_szyszka.jpg"):
+  
+    mydb = mysql.connector.connect(
+     host="185.253.218.123",
+     user="wwmdhvjb_student",
+     password="dzikaszyszk@2024",
+     database="wwmdhvjb_duckyfm"
+    )
+    
+    
+    
+
+#wykrzaczacz    
+else:
+    exit(1)
 
 
-mycursor = mydb.cursor()
-#mycursor.execute("CREATE TABLE users (name VARCHAR(27), surname VARCHAR(51))")
+
+
+
 
