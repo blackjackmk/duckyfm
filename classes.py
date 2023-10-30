f25120d53432e357d3a6e0f6bbac246de77bba7e
blackjackmk <kavalchukmaksum@gmail.com>
2023-10-30 21:46:00 +0100

    create, edit and delete twórcy

--- classes.py
+++ classes.py
@@ -1,1 +1,20 @@
-#zamierzam zrobić użytkowanika jako object klasy z polami i funkcjami. I dalej operować na odzielnym objekcie.
+import sqlite3
+
+#database connect
+conn = sqlite3.connect("duckybase.db") #if don't exist - create
+db = conn.cursor()
+
+class Artist:
+    id = 0
+    def __init__(self, pseudonim, opis): # is always executed when the class is being initiated
+        self.pseudonim = pseudonim
+        self.opis = opis
+    #tworzenie
+    def create(self):
+        query = "INSERT INTO tworcy ('pseudonim', 'description') VALUES (?, ?)"
+        db.execute(query, (self.pseudonim, self.opis))
+        conn.commit()
+    def update(self):
+        query = "UPDATE tworcy SET 'pseudonim' = ?, 'description' = ? WHERE artist_id = ?"
+        db.execute(query, (self.pseudonim, self.opis, self.id))
+        conn.commit()
--- logic.py
+++ logic.py
@@ -1,6 +1,8 @@
 import sqlite3
 import hashlib
 
+from classes import Artist
+
 #database connect
 conn = sqlite3.connect("duckybase.db") #if don't exist - create
 db = conn.cursor()
@@ -12,12 +14,34 @@ def logowanie(login, password):
     result = db.fetchone()
     hash = hashlib.sha256()
     hash.update(password.encode())
-    szyfred = hash.hexdigest()#zaszyfrowane hasło
+    szyfred = hash.hexdigest()  #zaszyfrowane hasło
     if szyfred == result[3]:
         print("login success")
     else:
         print("login error")
 
-#logowanie('kaczor','pass')
-#conn.commit()
+#tworzenie twórcy
+def create_artist(name, description):
+    artist = Artist(name, description)
+    artist.create()
+
+#edytowanie twórcy
+def edit_artist(id):
+    #tworzymy object z istniejącego artysty
+    query = "SELECT * FROM tworcy WHERE artist_id = ?"
+    db.execute(query, (id,))
+    result = db.fetchone()
+    artist = Artist(result[2], result[1])
+    artist.id = result[0]
+    #zmieniamy potrzebne dane
+    ##artist.pseudonim = "Nowe imie"
+    #zapisujemy zmiany
+    artist.update()
+
+#usunięcie twórcy
+def delete_artist(id):
+    query = "DELETE FROM tworcy WHERE artist_id = ?"
+    db.execute(query, (id,))
+    conn.commit()
+
 db.close()
