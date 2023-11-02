37eadb0b2262aaed5cad2797b29e974cc662415b
blackjackmk <kavalchukmaksum@gmail.com>
2023-11-02 13:13:32 +0100

    tworcy edit every field

--- classes.py
+++ classes.py
@@ -5,15 +5,19 @@ conn = sqlite3.connect("duckybase.db") #if don't exist - create
 db = conn.cursor()
 
 class Artist:
-    id = 0
-    def __init__(self, pseudonim, opis): # is always executed when the class is being initiated
+    def __init__(self, pseudonim, opis, id=None): # is always executed when the class is being initiated
         self.pseudonim = pseudonim
         self.opis = opis
+        self.id = id
+
     #tworzenie
     def create(self):
         query = "INSERT INTO tworcy ('pseudonim', 'description') VALUES (?, ?)"
         db.execute(query, (self.pseudonim, self.opis))
         conn.commit()
+        self.id = db.lastrowid
+
+    #edytowanie
     def update(self):
         query = "UPDATE tworcy SET 'pseudonim' = ?, 'description' = ? WHERE artist_id = ?"
         db.execute(query, (self.pseudonim, self.opis, self.id))
--- logic.py
+++ logic.py
@@ -26,17 +26,29 @@ def create_artist(name, description):
     artist.create()
 
 #edytowanie twórcy
-def edit_artist(id):
+def edit_artist(id, **args):
     #tworzymy object z istniejącego artysty
     query = "SELECT * FROM tworcy WHERE artist_id = ?"
     db.execute(query, (id,))
     result = db.fetchone()
-    artist = Artist(result[2], result[1])
-    artist.id = result[0]
-    #zmieniamy potrzebne dane
-    ##artist.pseudonim = "Nowe imie"
+    artist = Artist(result[1], result[2], result[0])
+    #zmieniamy potrzebne atrybuty objektu
+    # Definiujemy listę pól, które chcemy zaktualizować.
+    fields_to_update = []
+
+    # Pobieramy wartości pól do zmiany z argumentów funkcji.
+    for field, value in args.items():
+        # Dodajemy pole do listy pól do aktualizacji.
+        fields_to_update.append(field)
+
+    # Aktualizujemy pola w obiekcie artysty.
+    for field in fields_to_update:
+        artist.__setattr__(field, value)
     #zapisujemy zmiany
     artist.update()
+
+# Zmieniamy opis
+#edit_artist(1, opis="Nowy opis")
 
 #usunięcie twórcy
 def delete_artist(id):
