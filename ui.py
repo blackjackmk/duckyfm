ce0078bf6cd62b86812a87272182836d0fda671d
Michu <121090788+Michuj@users.noreply.github.com>
2023-11-23 10:55:58 +0100

    Update ui.py

--- ui.py
+++ ui.py
@@ -6,7 +6,7 @@ from googletrans import Translator
 translator = Translator()
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_default_color_theme('#A9E37C')  # Themes: "blue" (standard), "green", "dark-blue"
 
 class App(customtkinter.CTk):
     def __init__(self):
