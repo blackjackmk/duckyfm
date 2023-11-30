a0010c2bc19bdf8fe836e198e3053ae875eb16ed
blackjackmk <127087289+blackjackmk@users.noreply.github.com>
2023-11-30 10:36:39 +0100

    rejestracja

--- login.py
+++ login.py
@@ -1,4 +1,5 @@
 import hashlib
+import re
 
 from default_base import db, conn
 from classes import User, Admin
@@ -22,9 +23,31 @@ def logowanie(login, password):
     else:
         print("login error")
 
-#register_user
+def rejestracja(username, name, surname, haslo, haslo2):
+    #sprawdzamy wszystkie warunki
+    #regex = r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
+    #if (re.match(regex, email) == False):
+    #    print("Podaj poprawny email")
+    #    return False
+    if (haslo != haslo2):
+        print("Hasła nie zgadzają się")
+        return False
+    #pierwszy zarejestrowany uzytkownik staje sie administratorem systemu
+    db.execute("SELECT * FROM users")
+    results = cursor.fetchone()
+    if results is None:
+        is_admin = 1
+    else:
+        is_admin = 0
+    #wpisujemy użytkownika w bazie
+    query = "INSERT INTO users ('username', 'name', 'surname', 'is_admin', 'haslo') VALUES (?, ?, ?, ?, ?)"
+    hash = hashlib.sha256()
+    hash.update(haslo.encode())
+    haslo_zaszyfrowane = hash.hexdigest()
+    db.execute(query, (username, name, surname, is_admin, haslo_zaszyfrowane))
 
-while(CurrentUser == None):
+
+while (CurrentUser == None):
     login = input("Podaj login: ")
     password = input("Podaj haslo: ")
     logowanie(login, password)
