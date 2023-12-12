e245260a45ca9d4aba9580f838fedfbe4d6a94d7
blackjackmk <kavalchukmaksum@gmail.com>
2023-12-12 16:41:05 +0100

    create default tables

--- default_base.py
+++ default_base.py
@@ -1,60 +1,67 @@
-artist_table = '''CREATE TABLE tworcy (
-    artist_id   INTEGER   PRIMARY KEY AUTOINCREMENT,
-    pseudonim   TEXT (25),
-    description TEXT (50) );'''
+artist_table = '''CREATE TABLE tworcy ( 
+    artist_id INT IDENTITY PRIMARY KEY, 
+    pseudonim NVARCHAR (25), 
+    description NVARCHAR (50) )'''
 utwory_table = '''CREATE TABLE utwory (
-    song_id    INTEGER   PRIMARY KEY AUTOINCREMENT,
-    title      TEXT (30),
-    genre      INTEGER   REFERENCES genre (id_genre) ON UPDATE CASCADE,
-    artist     INTEGER   REFERENCES tworcy (artist_id) ON DELETE CASCADE
-                                                       ON UPDATE CASCADE,
-    album                REFERENCES plyty (album_id) ON DELETE CASCADE
-                                                     ON UPDATE CASCADE,
-    status     TEXT      DEFAULT Published,
+    song_id    INT IDENTITY PRIMARY KEY,
+    title      NVARCHAR (30),
+    genre      INT   REFERENCES genre (id_genre) ON UPDATE CASCADE,
+    artist     INT   REFERENCES tworcy (artist_id) ON DELETE CASCADE
+                                                    ON UPDATE CASCADE,
+    album      INT   REFERENCES plyty (album_id) ON DELETE NO ACTION,
+    status     NVARCHAR(12)      DEFAULT 'Published',
     created_at DATE
-);
-'''
+)'''
 plyty_table = '''CREATE TABLE plyty (
-    album_id    INTEGER   PRIMARY KEY AUTOINCREMENT,
-    title       TEXT (30),
-    description TEXT (60),
-    genre       INTEGER   REFERENCES genre (id_genre) ON UPDATE CASCADE
-);'''
+    album_id    INT IDENTITY PRIMARY KEY,
+    title       NVARCHAR (30),
+    description NVARCHAR (60),
+    genre       INT   REFERENCES genre (id_genre) ON UPDATE CASCADE
+)'''
 users_table = '''CREATE TABLE users (
-    user_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, 
-    username TEXT (30), 
-    name TEXT (60), 
-    surname TEXT (80), 
-    email TEXT (40),
-    adress TEXT, 
-    is_admin INTEGER (1) DEFAULT (0), 
-    haslo TEXT NOT NULL);'''
+    user_id INT IDENTITY PRIMARY KEY, 
+    username NVARCHAR (30), 
+    name NVARCHAR (60), 
+    surname NVARCHAR (80), 
+    email NVARCHAR (40),
+    adress NVARCHAR, 
+    is_admin INT DEFAULT (0), 
+    haslo NVARCHAR NOT NULL);'''
 genre = '''CREATE TABLE genre (
-    id_genre INTEGER PRIMARY KEY AUTOINCREMENT,
-    title    TEXT    NOT NULL
+    id_genre INT IDENTITY PRIMARY KEY,
+    title    NVARCHAR    NOT NULL
                      UNIQUE
 );'''
-connect_table = '''CREATE TABLE wykonawcy_utwory (
-    connect_id   INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_wykonawcy         REFERENCES tworcy (artist_id) ON DELETE CASCADE,
-    id_utworu            REFERENCES utwory (song_id) 
+wykonawcy_utwory = '''CREATE TABLE wykonawcy_utwory (
+    connect_id   INT IDENTITY PRIMARY KEY,
+    id_wykonawcy        INT REFERENCES tworcy (artist_id) ON DELETE CASCADE,
+    id_utworu           INT REFERENCES utwory (song_id) 
 );'''
 
-import sqlite3
+import pypyodbc as odbc
+from credential import db_login, db_pass
 
-#database connect
-conn = sqlite3.connect("duckybase.db") #if don't exist - create 
+conn = odbc.connect(
+  SERVER="duckybase.database.windows.net",
+  DATABASE="DuckyBase",
+  UID = db_login,
+  PWD = db_pass,
+  DRIVER='{ODBC Driver 18 for SQL Server}',
+  Trusted_Connection="no"
+)
+
 db = conn.cursor()
 
+
+
 try:
-    db.execute("SELECT * FROM users") # pozwala sprawdzić czy istnieją tabele, czy stworzona pusta baza
-except sqlite3.OperationalError:
-    #print("Nie ma takiej tabeli")
-    #tworzymy wszystkie tabele
-    db.execute(genre)
+    db.execute("SELECT * FROM users")
+except odbc.ProgrammingError:
+    #raise Exception("Brak tabeli users")
     db.execute(artist_table)
-    db.execute(utwory_table)
-    db.execute(plyty_table)
     db.execute(users_table)
-    db.execute(connect_table)
-    #możemy równierz wypełnić ich tutaj domyślnymi wartościami
+    db.execute(genre)
+    db.execute(plyty_table)
+    db.execute(utwory_table)
+    db.execute(wykonawcy_utwory)
+    conn.commit()
